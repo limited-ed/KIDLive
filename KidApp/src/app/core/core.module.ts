fe01239997b87f38825163a7614d023586c8e78e
limited-ed <yarosroman@gmail.com>
2021-02-24 06:26:54 +0300

    Update to ng10

--- KidApp/src/app/core/core.module.ts
+++ KidApp/src/app/core/core.module.ts
@@ -31,7 +31,7 @@ export class CoreModule {
     }
   }*/
 
-  static forRoot(): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders<CoreModule> {
     return {
       ngModule: CoreModule,
       providers: [
